4470c096db65662b974cd5a7337f3dea2f468fae
gdmec07150809 <672423400@qq.com>
2016-12-12 19:29:31 +0800

    qqq

--- app/src/main/java/com/example/administrator/app/MainActivity.java
+++ app/src/main/java/com/example/administrator/app/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private AlertDialog.Builder builder;
     private TextView tView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
